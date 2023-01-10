e722f4581ecbcf953640b826e8e5a095439ebf43
Abaevsk94 <abaevsk@gmail.com>
2023-01-10 20:11:33 +0300

    Done

--- src/Homework12/Book.java
+++ src/Homework12/Book.java
@@ -1,4 +1,30 @@
 package Homework12;
 
 public class Book {
+    private final String title;
+    private final Author author;
+    private int publishingYear;
+
+    public Book(String title, Author author, int publishingYear) {
+        this.title = title;
+        this.author = author;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int getPublishingYear() {
+        return this.publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+
 }
--- src/Homework12/Main.java
+++ src/Homework12/Main.java
@@ -2,5 +2,12 @@ package Homework12;
 
 public class Main {
     public static void main(String[] args) {
+        Author alexandrPushkin = new Author ("Александр","Пушкин");
+        Author williamShakespeare = new Author("Уильям", "Шекспир");
+        Book onegin = new Book("Евгений Онегин", alexandrPushkin, 1830);
+        Book romeoAndJuliette = new Book("Ромео и Джульетта", williamShakespeare, 1597);
+        onegin.setPublishingYear(1833);
+        System.out.println(onegin.getAuthor().getFullName() + " - " + onegin.getTitle() + " - " + onegin.getPublishingYear());
+        System.out.println(romeoAndJuliette.getAuthor().getFullName() + " - " + romeoAndJuliette.getTitle() + " - " + romeoAndJuliette.getPublishingYear());
     }
 }
